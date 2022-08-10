c043c28a21e73fece35fdf55c36a451dab08cef7
Gayoun <makeappsgreat@gmail.com>
2022-08-10 23:25:21 +0900

    New project "demospringdata"
    Edit "README.md"
    New project "jdbcsample"

---
+++ spring-jpa/demospringdata/src/test/java/kr/makeappsgreat/demospringdata/DemospringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.makeappsgreat.demospringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
